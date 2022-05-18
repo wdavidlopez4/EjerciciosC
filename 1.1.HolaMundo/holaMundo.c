675f8e33d6aeed776a88d834316c4362c9578595
wdavidlopez4 <wdavidlopez4@gmail.com>
2022-05-18 12:21:17 -0500

    programas hola mundo

---
+++ 1.1.HolaMundo/holaMundo.c
@@ -0,0 +1,16 @@
+/**
+ * @file holaMundo.c
+ * @author william lopez
+ * @brief programa hola mundo
+ * @version 0.1
+ * @date 2022-05-18
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ * programa hola mundo
+ */
+#include<stdio.h>
+
+main(){
+    printf("Hola mundo \n");
+}
---
+++ 1.2.HolaMundo_con_caracter_c/holamundo.c
@@ -0,0 +1,17 @@
+/**
+ * @file holamundo.c
+ * @author william lopez
+ * @brief programa hola mundo
+ * @version 0.1
+ * @date 2022-05-18
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ * programa hola mundo, observamos que la caracter especial "\c" no se reconoce y el compilador imprimer "hola mundo c" 
+ */
+
+#include <stdio.h>
+
+main(){
+    printf("Hola mundo \c");
+}
